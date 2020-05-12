76651582ff7f15a8d989bc93ed57714c3b256a7b
Babatunde Olaniyi <bolaniyi.mvc@gmail.com>
2020-05-11 21:28:51 -0500

    create API endpoint

--- server.js
+++ server.js
@@ -7,10 +7,29 @@ const app = express();
 //Connect database
 connectDatabase();
 
+// Configuree Middleware 
+app.use(express.json ({ extended: false }));
+
+
 //API endpoints
+/**
+ * @route
+ * @desc test end point
+ */
+
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
+/**
+ * @route
+ * @desc test end point
+ */
+
+ app.post('/api/users', (req, res) => {
+     console.log(req.body);
+     res.send(req.body);
+ });
+ 
 //connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
