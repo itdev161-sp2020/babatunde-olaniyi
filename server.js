3877faf960cf48d06d39e51dbe8f5bfe472657d8
Babatunde Olaniyi <bolaniyi.mvc@gmail.com>
2020-05-12 21:16:04 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db.js';
+import { check, validationResult } from 'express-validator';
 
 //Initialize express application
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 connectDatabase();
 
 // Configuree Middleware 
-app.use(express.json ({ extended: false }));
+app.use(express.json({ extended: false }));
 
 
 //API endpoints
@@ -26,10 +27,27 @@ app.get('/', (req, res) =>
  * @desc test end point
  */
 
- app.post('/api/users', (req, res) => {
-     console.log(req.body);
-     res.send(req.body);
- });
+ app.post('/api/users', 
+ [
+     check('name', 'Please enter your name')
+     .not()
+     .isEmpty(),
+     check('email', 'Please enter a valid email').isEmail(),
+     check('password', 
+           'Please enter a password with 6 or more characters'
+           ).isLength({ min: 6 })
+ ],
+ (req, res) => {
+     const errors = validationResult(req);
+     if ( !errors.isEmpty()) {
+         return res.status(422).json ({ errors: errrors.array() });
+     } else {
+     return res.send(req.body)
+     }
+   }
+ );
  
+
+
 //connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
