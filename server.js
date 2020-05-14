fc9233acde27b3df457d62887cb07e37ff2bc090
Babatunde Olaniyi <bolaniyi.mvc@gmail.com>
2020-05-13 22:27:14 -0500

    import axios

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import connectDatabase from './config/db.js';
+import connectDatabase from '../config/db.js';
 import { check, validationResult } from 'express-validator';
 
 //Initialize express application
