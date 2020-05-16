a5f8fa34b07765d556a6eb32d672370eb90de9c7
Babatunde Olaniyi <bolaniyi.mvc@gmail.com>
2020-05-16 14:49:53 -0500

    Test the Delete Post Endpoint in Postman

--- server.js
+++ server.js
@@ -261,8 +261,87 @@ app.post(
       }
     }
   );
-  
+
+  /**
+ * @route GET api/posts
+ * @desc Get posts
+ */
+
+app.get(
+    '/api/posts',
+    auth,
+    async (req, res) => {
+        try {
+            const posts = await Post.find().sort({ date: -1 });
+
+            res.json(posts);
+        } catch (error) {
+            console.error.apply(error);
+            res.status(500).send('Server error');
+        }
+    }
+);
+
+/**
+ * @route GET api/posts/:id
+ * @desc Get post
+ */
+
+app.get(
+    '/api/posts/:id',
+    auth,
+    async (req, res) => {
+        try {
+            const post = await Post.findById(req.params.id);
+
+            //Make sure the post was found
+            if (!post) {
+                return res.status(404).json({ msg: 'Post not found'});
+            }
+
+            res.json(post);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Server error');
+        }
+    }
+);
+
+/**
+ * @route DELETE api/posts/:id
+ * @desc Delete a post
+ */
+app.delete(
+    '/api/posts/:id',
+    auth,
+    async (req, res) => {
+        try{
+            const post = await Post.findById(req.params.id);
+
+            //Make sure the post was found
+            if (!post) {
+                return res.status(404).json({ msg: 'Post not found' });
+            }
+
+            //make sure the request user created the post
+            if (post.user.toString() !== req.user.id) {
+                return res.status(401).json({ msg: 'User not authorized'});
+            }
+
+            await post.remove();
+
+            res.json({ msg: 'Post removed'});
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Server error');
+        }
+    }
+);
+
+    /*
     //connection listener
     //app.listen(3000, () => console.log('Express server running on port 3000'));
+    */
+
 const port = 5000;
 app.listen(port, () => console.log(`Express server running on port ${port}`));
